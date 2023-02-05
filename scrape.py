786c68e641bc7f739fa8142acf404925c688a59c
shar27 <shariq.ahmed731@gmail.com>
2023-02-05 16:24:49 +0100

    first commit

---
+++ scrape.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import requests 
+
+url ="https://www.google.com/search?q=estate+agents+wales+%22mailto%22&rlz=1C1CHBD_en-GBIT1013IT1013&biw=1440&bih=757&sxsrf=AJOqlzWnU1cNPFqHciS9V_f9r8YjRT6uUw%3A1674461433742&ei=-UDOY5PqLO387_UPrqGHkAs&ved=0ahUKEwjTysr3nt38AhVt_rsIHa7QAbIQ4dUDCA8&uact=5&oq=estate+agents+wales+%22mailto%22&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICCEQoAEQwwQyCAghEKABEMMEMggIIRCgARDDBDoKCAAQRxDWBBCwAzoICAAQCBAHEB5KBAhBGABKBAhGGABQrwtYohFg6CZoAnABeACAAZ0BiAGUBpIBAzAuNpgBAKABAcgBCMABAQ&sclient=gws-wiz-serp"
+
